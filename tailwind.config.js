7d6a6659c5bd051bf16ba6435ef2f81fb49d77f8
hosseinfs <hosseinfarhanisadr@gmail.com>
2021-04-22 01:41:57 +0430

    Add tailwind css dependencies and config files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    screens: {
+      xs: '475px',
+      ...defaultTheme.screens,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
