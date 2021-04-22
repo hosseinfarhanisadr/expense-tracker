a7627e261b48eecb5e169e503361c2848822593f
hosseinfs <hosseinfarhanisadr@gmail.com>
2021-04-22 05:27:01 +0430

    Add editInitialBalance redux action and selectInitialBalance selector

--- store/app/walletSlice.js
+++ store/app/walletSlice.js
@@ -3,7 +3,22 @@ import { createSlice } from '@reduxjs/toolkit';
 const transactionsSlice = createSlice({
   name: 'wallet',
   initialState: { initialBalance: 0 },
-  reducers: {},
+  reducers: {
+    editInitialBalance: (state, action) => {
+      const newInitialBalance = action.payload;
+      if (newInitialBalance >= 0) {
+        state.initialBalance = newInitialBalance;
+      }
+    },
+  },
 });
 
-export default transactionsSlice.reducer;
+export const selectInitialBalance = (state) => {
+  return state.wallet.initialBalance;
+};
+
+const { actions, reducer } = transactionsSlice;
+
+export const { editInitialBalance } = actions;
+
+export default reducer;
