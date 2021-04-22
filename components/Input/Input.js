3f8eb2b5f42e96c9663727bc3361da9a46a4b0a8
hosseinfs <hosseinfarhanisadr@gmail.com>
2021-04-22 05:10:12 +0430

    Add Input component

---
+++ components/Input/Input.js
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+
+const Input = ({
+  id,
+  name,
+  label,
+  type = 'text',
+  error = false,
+  multiline = false,
+  ...rest
+}) => {
+  const inputId = id ? id : name;
+  const Component = multiline ? 'textarea' : 'input';
+  return (
+    <>
+      {label && (
+        <label
+          htmlFor={inputId}
+          className="block text-gray-700 text-lg font-bold mb-2"
+        >
+          {label}
+        </label>
+      )}
+      <Component
+        id={inputId}
+        type={type}
+        name={name}
+        className={`appearance-none border border-gray-300 focus:outline-none p-3 rounded-md w-full ${
+          error ? 'border-red-500' : ''
+        }`}
+        {...rest}
+      />
+    </>
+  );
+};
+
+Input.propTypes = {
+  id: PropTypes.string,
+  error: PropTypes.bool,
+  name: PropTypes.string,
+  type: PropTypes.string,
+  label: PropTypes.string,
+  multiline: PropTypes.bool,
+};
+
+export default Input;
